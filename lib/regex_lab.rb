4d5410af3d3c975b1f40350ee8cbd1801525fbb2
Sara Harvey <saraharveynyc@gmail.com>
2018-09-02 01:10:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/\w+un-ing/)
+"unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/\w+un/)
 end
 
 def words_five_letters_long(text)

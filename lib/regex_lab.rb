7eb710b514b4e6b7eab43bb2985bc8b66bb5bbcd
Sara Harvey <saraharveynyc@gmail.com>
2018-09-02 01:15:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,7 @@ true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/\w+un/ing)
-end
+text.scan(/un\w+ing/)
 
 def words_five_letters_long(text)
 

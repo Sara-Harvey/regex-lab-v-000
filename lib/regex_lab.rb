73905ca7620ba2ee9fc91e770ade4606cf3bf314
Sara Harvey <saraharveynyc@gmail.com>
2018-09-02 01:09:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"Unending rain unrelenting sneezes".scan(/\w+un-ing/)
+"unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/\w+un-ing/)
 end
 
 def words_five_letters_long(text)
